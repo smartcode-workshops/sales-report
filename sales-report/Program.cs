3e275272176fc59dbd8325aff521dff496f453c4
Lei Xu <leixu@leansoftx.com>
2024-06-27 10:59:49 +0800

    init project

---
+++ sales-report/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReportGenerator
+{
+    class QuarterlyIncomeReport
+    {
+        static void Main(string[] args)
+        {
+            // create a new instance of the class
+            QuarterlyIncomeReport report = new QuarterlyIncomeReport();
+
+            // call the GenerateSalesData method
+
+            // call the QuarterlySalesReport method
+
+        }
+
+        public void QuarterlySalesReport()
+        {
+            Console.WriteLine("Quarterly Sales Report");
+        }
+
+
+    }    
+}
